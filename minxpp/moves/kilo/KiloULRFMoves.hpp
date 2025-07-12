269ba97a7c89c9eecc601de5616a65f0fb522631
LucaGoubelle <luca.goubelle97@gmail.com>
2025-07-12 20:14:13 +0200

    Bind KiloULRFMoves wih kilo mover

---
+++ minxpp/moves/kilo/KiloULRFMoves.hpp
@@ -0,0 +1,76 @@
+#pragma once
+#include "../../data/models/kilominx.hpp"
+
+class KiloULRFMoves {
+    public:
+        Kilominx moveU(Kilominx minx){
+            // todo: implement this
+            return minx;
+        }
+
+        Kilominx moveUPrime(Kilominx minx){
+            for(int i=0;i<3;i++)
+                minx = this->moveU(minx);
+            return minx;
+        }
+
+        Kilominx moveU2(Kilominx minx){
+            for(int i=0;i<2;i++)
+                minx = this->moveU(minx);
+            return minx;
+        }
+
+
+        Kilominx moveL(Kilominx minx){
+            // todo: implement this
+            return minx;
+        }
+
+        Kilominx moveLPrime(Kilominx minx){
+            for(int i=0;i<3;i++)
+                minx = this->moveL(minx);
+            return minx;
+        }
+
+        Kilominx moveL2(Kilominx minx){
+            for(int i=0;i<2;i++)
+                minx = this->moveL(minx);
+            return minx;
+        }
+
+
+        Kilominx moveR(Kilominx minx){
+            // todo: implement this
+            return minx;
+        }
+
+        Kilominx moveRPrime(Kilominx minx){
+            for(int i=0;i<3;i++)
+                minx = this->moveR(minx);
+            return minx;
+        }
+
+        Kilominx moveR2(Kilominx minx){
+            for(int i=0;i<2;i++)
+                minx = this->moveR(minx);
+            return minx;
+        }
+
+
+        Kilominx moveF(Kilominx minx){
+            // todo: implement this
+            return minx;
+        }
+
+        Kilominx moveFPrime(Kilominx minx){
+            for(int i=0;i<3;i++)
+                minx = this->moveF(minx);
+            return minx;
+        }
+
+        Kilominx moveF2(Kilominx minx){
+            for(int i=0;i<2;i++)
+                minx = this->moveF(minx);
+            return minx;
+        }
+};
--- minxpp/moves/kilo/KilominxMover.hpp
+++ minxpp/moves/kilo/KilominxMover.hpp
@@ -1,12 +1,31 @@
 #pragma once
 #include "../../data/models/kilominx.hpp"
 #include "../minx_mover.hpp"
+#include "KiloULRFMoves.hpp"
 
 class KilominxMover : public MinxMover {
+    private:
+        KiloULRFMoves ulrfMoves;
     public:
+        KilominxMover(){
+            KiloULRFMoves ulrfMoves;
+        }
+
         Kilominx simpleMove(Kilominx minx, std::string mv){
-            //todo: implement this
-            return minx;
+            if(mv=="U") return this->ulrfMoves.moveU(minx);
+            else if(mv=="U'") return this->ulrfMoves.moveUPrime(minx);
+            else if(mv=="U2") return this->ulrfMoves.moveU2(minx);
+            else if(mv=="L") return this->ulrfMoves.moveL(minx);
+            else if(mv=="L'") return this->ulrfMoves.moveLPrime(minx);
+            else if(mv=="L2") return this->ulrfMoves.moveL2(minx);
+            else if(mv=="R") return this->ulrfMoves.moveR(minx);
+            else if(mv=="R'") return this->ulrfMoves.moveRPrime(minx);
+            else if(mv=="R2") return this->ulrfMoves.moveR2(minx);
+            else if(mv=="F") return this->ulrfMoves.moveF(minx);
+            else if(mv=="F'") return this->ulrfMoves.moveFPrime(minx);
+            else if(mv=="F2") return this->ulrfMoves.moveF2(minx);
+            //todo: add axis moves
+            else return minx;
         }
 
         Kilominx multiMoves(Kilominx minx, std::string mvs){
