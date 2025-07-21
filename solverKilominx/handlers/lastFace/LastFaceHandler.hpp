9d927d993ac5638ca65da0fbdffa79028df98ff1
LucaGoubelle <luca.goubelle97@gmail.com>
2025-07-21 18:10:02 +0200

    Prepare processors in solver kilominx

---
+++ solverKilominx/handlers/lastFace/LastFaceHandler.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "../../../minxpp/data/models/kilominx.hpp"
+
+class LastFaceHandler {
+    public:
+        Kilominx handle(Kilominx minx){
+            // implement this
+            return minx;
+        }
+};
