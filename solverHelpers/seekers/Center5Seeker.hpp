5dff3fe7b72ea4676492d647d8a41ccbaf2718fd
LucaGoubelle <luca.goubelle97@gmail.com>
2025-07-22 21:16:27 +0200

    Implement more in Center5Seeker

--- solverHelpers/seekers/Center5Seeker.hpp
+++ solverHelpers/seekers/Center5Seeker.hpp
@@ -29,7 +29,24 @@ class Center5Seeker {
         }
 
         std::string seekCenterCorner(Cube cube, std::string face, std::string color){
-            //todo: implement this
-            return "???";
+            std::map<std::string, std::string> hmapFront;
+            hmapFront["front::up::right"] = cube.front[1][3];
+            hmapFront["front::up::left"] = cube.front[1][1];
+            hmapFront["front::down::right"] = cube.front[3][3];
+            hmapFront["front::down::left"] = cube.front[3][1];
+
+            std::map<std::string, std::string> hmapUp;
+            hmapUp["up::front::left"] = cube.up[3][1];
+            hmapUp["up::front::right"] = cube.up[3][3];
+            hmapUp["up::back::left"] = cube.up[1][1];
+            hmapUp["up::back::right"] = cube.up[1][3];
+
+            if(face=="front")
+                for(auto const& x : hmapFront)
+                    if(x.second==color) return x.first;
+            else if (face=="up")
+                for(auto const& x : hmapUp)
+                    if(x.second == color) return x.first;
+            else return "???";
         }
 };
