14127bb128199c8b075c5511da8afa1d8bc42012
LucaGoubelle <luca.goubelle97@gmail.com>
2025-07-23 16:21:06 +0200

    Improve accuracy of center5seeker

--- solverHelpers/seekers/Center5Seeker.hpp
+++ solverHelpers/seekers/Center5Seeker.hpp
@@ -19,11 +19,29 @@ class Center5Seeker {
             hmapUp["up::right"] = cube.up[2][3];
             hmapUp["up::back"] = cube.up[1][2];
 
+            std::map<std::string, std::string> hmapBack;
+            hmapBack["back::up"] = cube.back[1][2];
+            hmapBack["back::down"] = cube.back[3][2];
+            hmapBack["back::right"] = cube.back[2][1];
+            hmapBack["back::left"] = cube.back[2][3];
+
+            std::map<std::string, std::string> hmapDown;
+            hmapDown["down::front"] = cube.down[1][2];
+            hmapDown["down::back"] = cube.down[3][2];
+            hmapDown["down::left"] = cube.down[2][1];
+            hmapDown["down::right"] = cube.down[2][3];
+
             if(face=="front")
                 for(auto const& x : hmapFront)
                     if(x.second==color) return x.first;
             else if (face=="up")
                 for(auto const& x : hmapUp)
+                    if(x.second == color) return x.first;
+            else if(face=="back")
+                for(auto const& x : hmapBack)
+                    if(x.second == color) return x.first;
+            else if(face=="down")
+                for(auto const& x : hmapDown)
                     if(x.second == color) return x.first;
             else return "???";
         }
@@ -41,11 +59,29 @@ class Center5Seeker {
             hmapUp["up::back::left"] = cube.up[1][1];
             hmapUp["up::back::right"] = cube.up[1][3];
 
+            std::map<std::string, std::string> hmapBack;
+            hmapBack["back::up::right"] = cube.back[1][1];
+            hmapBack["back::up::left"] = cube.back[1][3];
+            hmapBack["back::down::right"] = cube.back[3][1];
+            hmapBack["back::down::left"] = cube.back[3][3];
+
+            std::map<std::string, std::string> hmapDown;
+            hmapDown["down::front::left"] = cube.down[1][1];
+            hmapDown["down::front::right"] = cube.down[1][3];
+            hmapDown["down::back::left"] = cube.down[3][1];
+            hmapDown["down::back::right"] = cube.down[3][3];
+
             if(face=="front")
                 for(auto const& x : hmapFront)
                     if(x.second==color) return x.first;
             else if (face=="up")
                 for(auto const& x : hmapUp)
+                    if(x.second == color) return x.first;
+            else if (face=="back")
+                for(auto const& x : hmapBack)
+                    if(x.second == color) return x.first;
+            else if (face=="down")
+                for(auto const& x : hmapDown)
                     if(x.second == color) return x.first;
             else return "???";
         }
