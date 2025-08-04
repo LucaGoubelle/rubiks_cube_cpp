ea1693ca0d9bb8c1bccdb2f2760a951ed013288e
LucaGoubelle <luca.goubelle97@gmail.com>
2025-08-04 21:09:10 +0200

    Bind white center handler with subhandler

--- solver5x5/handlers/centers/white/SideWhiteBarHandler.hpp
+++ solver5x5/handlers/centers/white/SideWhiteBarHandler.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "../../../../rubikpp/data/cube.hpp"
+#include "../../../../rubikpp/moves/mover.hpp"
+#include "../../../../solverHelpers/seekers/Center5Seeker.hpp"
 
 class SideWhiteBarHandler {
     private:
