698f1b37a9fb8c1c18181a474a0a91c049e2a03c
LucaGoubelle <luca.goubelle97@gmail.com>
2026-02-28 16:59:52 +0100

    Add processors for first layer handler in solver3x3

---
+++ solversCube/solver3x3/processors/wbr_processor.hpp
@@ -0,0 +1,51 @@
+#pragma once
+#include <map>
+#include <string>
+#include <algorithm>
+
+class WBRProcessor {
+    private:
+        std::map<std::string, std::string> data;
+    public:
+        WBRProcessor(){
+            this->data["up_front_right::white_red_blue"] = "R U R' U' R U R' U' R U R' U'";
+            this->data["up_front_right::blue_white_red"] = "U R U' R'";
+            this->data["up_front_right::red_blue_white"] = "R U R'";
+
+            this->data["up_front_left::white_blue_red"] = "R U2 R' U R U' R'";
+            this->data["up_front_left::blue_red_white"] = "R U' R'";
+            this->data["up_front_left::red_white_blue"] = "U' R U R'";
+
+            this->data["up_back_right::white_blue_red"] = "U' R U R' U R U' R'";
+            this->data["up_back_right::blue_red_white"] = "U2 R U' R'";
+            this->data["up_back_right::red_white_blue"] = "U R U R'";
+
+            this->data["up_back_left::white_red_blue"] = "U2 R U R' U' R U R' U' R U R' U'";
+            this->data["up_back_left::blue_white_red"] = "U' R U' R'";
+            this->data["up_back_left::red_blue_white"] = "U2 R U R'";
+            //down
+            this->data["down_front_right::red_white_blue"] = "R U' R' U R U' R'";
+            this->data["down_front_right::blue_red_white"] = "R U R' U' R U R' U'";
+
+            this->data["down_front_left::white_red_blue"] = "L' U' L R U R'";
+            this->data["down_front_left::blue_white_red"] = "L' U' L R U R' U' R U R' U' R U R' U'";
+            this->data["down_front_left::red_blue_white"] = "L' U' L U R U' R'";
+
+            this->data["down_back_left::white_blue_red"] = "L U2 L' U R U' R'";
+            this->data["down_back_left::red_white_blue"] = "L U' L' R U2 R'";
+            this->data["down_back_left::blue_red_white"] = "L U2 L' R U R'";
+
+            this->data["down_back_right::white_red_blue"] = "R' U2 R U' R U R'";
+            this->data["down_back_right::blue_white_red"] = "R' U R U R U R'";
+            this->data["down_back_right::red_blue_white"] = "R' U2 R2 U' R'";
+        }
+
+        /**
+         * Return the matching sequence of moves based on 
+         * inputData param
+         * @author: LucaGoubelle
+         */
+        std::string process(std::string inputData){
+            return (this->data.count(inputData)) ? this->data[inputData] : "???";
+        }
+};
